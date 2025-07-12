f19b3c5af968e3ed38e1b06b5701db0a7365fa0e
DavidRM <david123ramirez@hotmail.com>
2025-07-12 19:45:22 +0200

    Finalización de la lógica de entrada de noticias

--- blogging/src/app/components/blog/blog.component.ts
+++ blogging/src/app/components/blog/blog.component.ts
@@ -5,7 +5,7 @@ import { HtmlParser } from '@angular/compiler';
 
 @Component({
   selector: 'app-blog',
-  imports: [FormsModule],
+  imports: [FormsModule,],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
@@ -16,8 +16,20 @@ export class BlogComponent {
       textArea : '',
       date : ''
     }
-    blog : Array<INoticia> = [];
-
+    blog : Array<INoticia> = [
+      {
+        title: "La tarta de la madre de Cris: 'Dominaremos el mundo'",
+        url: 'evil-cheese-cake.png',
+        textArea: 'Quien digo que las elites dominaban el mundo porque ahora una simple pero deliciosa tarta de queso tiene al mundo bajo sus pies.',
+        date: '2025-07-12'
+      },
+      {
+        title: 'Algas como sombrero: NUEVA MODA',
+        url: 'alga-sombrero.png',
+        textArea: 'No permitas que el calentamiento global te queme los pensamientos. Con esta nueva moda te refrescas y eres ecológica.',
+        date: '2025-07-10'
+      }
+    ];
     cargarNoticia():void{
       if(this.noticia.title !=='' && this.noticia.url !=='' &&
          this.noticia.textArea !=='' && this.noticia.date !==''){
@@ -26,12 +38,28 @@ export class BlogComponent {
           this.noticia[key as keyof INoticia] = '';
         })
         ;
+        } else {
+          alert("No has introducido todo correctamente");
         }
     }
 
     pintarNoticias(){
       let html = '';
 
+      this.blog.forEach(notice => {
+        html = html + 
+        `<div class="news-card">
+          <div class="news-card__wrapper">
+            <img src="${notice.url}" alt="" class="wrapper_img">
+          </div>
+          <div class="news-card__body">
+            <h4 class="news-card__title">${notice.title}</h4>
+            <p class="news-card__text justify">${notice.textArea}</p>
+          <span class="news-card__date left">${notice.date}</span>
+          </div>
+        </div>`; 
+        }
+      );
       return html;
     }
 }
