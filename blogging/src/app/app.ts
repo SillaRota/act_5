c1f9c94667962ec5bada920a6df03374eb8863de
DavidRM <david123ramirez@hotmail.com>
2025-07-12 13:38:46 +0200

    Estructuración interna blog (Vista)

--- blogging/src/app/app.ts
+++ blogging/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BlogComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
